df12de5e46199c686ba65bd95b77809d6f01a871
mbecker20 <becker.maxh@gmail.com>
2023-03-11 22:35:42 +0000

    readme

---
+++ readme.md
@@ -0,0 +1,35 @@
+# SVI
+## *string variable interpolator*
+
+this crate contains a function that interpolates variables in a hashmap into an input string, producing both the resulting string, plus a readable string that is safe to store or print without compromising secrets.
+
+variables can be matched with either ```[[variable_name]]``` (DoubleBrackets) or ```{{variable_name}}``` (DoubleCurlyBrackets).
+
+## Usage
+```rust
+let variables: HashMap<String, String> = [
+	("mongo_username", "root"),
+	("mongo_password", "mng233985725"),
+]
+.into_iter()
+.map(|(k, v)| (k.to_string(), v.to_string()))
+.collect();
+
+let to_fmt = "mongodb://[[mongo_username]]:[[mongo_password]]@127.0.0.1:27017";
+
+let (formatted, readable) = svi::interpolate_variables(to_fmt, &variables, svi::Interpolator::DoubleBrackets)?;
+
+println!("{formatted}"); // prints 'mongodb://root:mng233985725@127.0.0.1:27017'
+println!("{readable}"); // prints 'mongodb://<mongo_username>:<mongo_password>@127.0.0.1:27017'
+```
+
+## Escaping Interpolation
+```rust
+let variables = HashMap::new();
+
+let to_fmt = "the interpolator will escape interpolation with 3 openers: [[[not a variable]]]";
+
+let (formatted, _) = svi::interpolate_variables(to_fmt, &variables, svi::Interpolator::DoubleBrackets)?;
+
+println!("{formatted}"); // prints 'the interpolator will escape interpolation with 3 openers: [[not a variable]]'
+```
--- src/lib.rs
+++ src/lib.rs
@@ -12,23 +12,25 @@ pub fn interpolate_variables(
     input: &str,
     variables: &HashMap<String, String>,
     interpolator: Interpolator,
-) -> Result<(String, String), VariableInterpolatorError> {
+) -> Result<(String, String), SviError> {
     let mut result = String::new();
     let mut replace_for_readable = Vec::new();
 
-    let (double_opener, single_opener, triple_closer, double_closer) =
-        match interpolator {
-            Interpolator::DoubleCurlyBrackets => ("{{", "{", "}}}", "}}"),
-            Interpolator::DoubleBrackets => ("[[", "[", "]]]", "]]"),
-        };
+    let (double_opener, single_opener, triple_closer, double_closer) = match interpolator {
+        Interpolator::DoubleCurlyBrackets => ("{{", "{", "}}}", "}}"),
+        Interpolator::DoubleBrackets => ("[[", "[", "]]]", "]]"),
+    };
 
     let mut open_split = input.split(double_opener).collect::<VecDeque<_>>();
-    let first = open_split
-        .pop_front()
-        .ok_or(VariableInterpolatorError::SplitEmpty)?;
+    let first = open_split.pop_front().ok_or(SviError::SplitEmpty)?;
     result.push_str(first);
 
     for i in 0..open_split.len() {
+        if open_split[i].get(0..1).is_none() {
+            return Err(SviError::UsedDoubleOpener {
+                double_opener: double_opener.to_string(),
+            });
+        }
         if &open_split[i][0..1] == single_opener {
             result.push_str(single_opener);
             let split2 = open_split[i].split(triple_closer).collect::<Vec<_>>();
@@ -41,12 +43,12 @@ pub fn interpolate_variables(
         } else {
             let split2 = open_split[i].split(double_closer).collect::<Vec<_>>();
             if split2.len() <= 1 {
-                return Err(VariableInterpolatorError::NoClosingTags { index: i });
+                return Err(SviError::NoClosingTags { index: i });
             }
             let variable = split2[0];
-            let value = variables
-                .get(variable)
-                .ok_or(VariableInterpolatorError::NoValueFound { variable: variable.to_string() })?;
+            let value = variables.get(variable).ok_or(SviError::NoValueFound {
+                variable: variable.to_string(),
+            })?;
             replace_for_readable.push((value.clone(), format!("<{variable}>")));
             result.push_str(value);
             result.push_str(&split2[1..].join(""));
@@ -63,15 +65,13 @@ pub fn interpolate_variables(
 }
 
 #[derive(Error, Debug)]
-pub enum VariableInterpolatorError {
+pub enum SviError {
     #[error("split is empty. this shouldn't happen")]
     SplitEmpty,
-	#[error("no closing tags for variable at index {index}")]
-	NoClosingTags {
-		index: usize,
-	},
-	#[error("did not find any value for variable {variable}")]
-	NoValueFound {
-		variable: String
-	}
+    #[error("found double opener '{double_opener}{double_opener}', formatting will fail")]
+    UsedDoubleOpener { double_opener: String },
+    #[error("no closing tags for variable at index {index}")]
+    NoClosingTags { index: usize },
+    #[error("did not find any value for variable {variable}")]
+    NoValueFound { variable: String },
 }
